c1ac2531ad1956b04689372e1737859e3835ce8f
gongshun <gongshun@gridsum.com>
2020-07-13 10:52:24 +0800

    feat：使用 loadMicroApp 来实现keep-alive

--- main/src/App.vue
+++ main/src/App.vue
@@ -6,17 +6,53 @@
       <router-link to="/about">about</router-link>
       <span @click="changeParentState">主项目的数据：{{ commonData.parent }}，点击变回1</span>
     </header>
-    <div id="appContainer"></div>
+    <div id="appContainer1" v-show="$route.path.startsWith('/app-vue-hash/')"></div>
+    <div id="appContainer2" v-show="$route.path.startsWith('/app-vue-history/')"></div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { loadMicroApp } from 'qiankun';
+import router from './router'
+import store from './store'
+
+const apps = [
+  { 
+    name: 'app-vue-hash', 
+    entry: 'http://localhost:1111', 
+    container: '#appContainer1',
+    props: { data : { store, router } }
+  },
+  { 
+    name: 'app-vue-history',
+    entry: 'http://localhost:2222', 
+    container: '#appContainer2',
+    props: { data : store }
+  }
+]
+
 export default {
   computed: {
     commonData(){
       return this.$store.state.commonData;
     }
+  },
+  mounted() {
+    // 优先加载当前的子项目
+    const path = this.$route.path;
+    console.log(path);
+    const currentAppIndex = apps.findIndex(item => path.includes(item.name));
+    console.log(currentAppIndex);
+    if(currentAppIndex !== -1){
+      const currApp = apps.splice(currentAppIndex, 1)[0];
+      apps.unshift(currApp);
+    }
+    console.log(apps);
+    // loadMicroApp 返回值是 app 的生命周期函数数组
+    const loadApps = apps.map(item => loadMicroApp(item))
+    console.log(loadApps);
+    // 当 tab 页关闭时，调用 loadApps 中 app 的 unmount 函数即可
   },
   methods: {
     changeParentState(){
--- main/src/main.js
+++ main/src/main.js
@@ -3,7 +3,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { registerMicroApps, start } from 'qiankun';
 
 Vue.config.productionTip = false
 
@@ -12,22 +11,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
-
-registerMicroApps([
-  { 
-    name: 'app-vue-hash', 
-    entry: 'http://localhost:1111', 
-    container: '#appContainer', 
-    activeRule: '/app-vue-hash', 
-    props: { data : { store, router } }
-  },
-  { 
-    name: 'app-vue-history',
-    entry: 'http://localhost:2222', 
-    container: '#appContainer', 
-    activeRule: '/app-vue-history',
-    props: { data : store }
-  },
-]);
-
-start();
