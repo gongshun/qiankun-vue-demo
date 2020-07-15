4a008e27ec6f89d1d91d0a5b9ffe40098f9ccd7e
gongshun <gongshun@gridsum.com>
2020-07-15 18:13:30 +0800

    feat：去掉无用代码

--- main/vue.config.js
+++ main/vue.config.js
@@ -2,8 +2,5 @@ const path = require('path');
 
 module.exports = {
   transpileDependencies: ['single-spa','qiankun','import-html-entry'],
-  chainWebpack: config => {
-    config.resolve.alias.set('./patchers', path.resolve(__dirname, 'src/assets/patchers.js'))
-  }
 };
 
